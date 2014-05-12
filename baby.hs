9eee2151687ad522ef3f95058bad6c41a147d29c
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-05-12 23:37:37 +1000

    Add BMI diagnosis function using where

--- baby.hs
+++ baby.hs
@@ -14,3 +14,11 @@ myZip (x:xs) (y:ys) = (x, y) : (myZip xs ys)
 
 enumerate :: [a] -> [(Int, a)]
 enumerate xs = zip [0..] xs
+
+bmiDiagnosis :: (RealFloat a) => a -> a -> String
+bmiDiagnosis weight height
+    | bmi <= 18.5 = "Underweight"
+    | bmi <= 25.0 = "Normal"
+    | bmi <= 30.0 = "Overweight"
+    | otherwise   = "Obese"
+    where bmi = weight / height^2
