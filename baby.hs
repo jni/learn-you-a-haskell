75657903baa3af577aff1a6735f8d591dcd66c73
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-05-12 16:43:55 +1000

    Add a few function defs

--- baby.hs
+++ baby.hs
@@ -2,3 +2,15 @@ doubleMe x = x + x
 
 doubleUs [] = []
 doubleUs (x:xs) = doubleMe x : doubleUs xs
+
+upperCase c = c `elem` ['A'..'Z']
+
+lowerCase c = c `elem` ['a'..'z']
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] ys = []
+myZip xs [] = []
+myZip (x:xs) (y:ys) = (x, y) : (myZip xs ys)
+
+enumerate :: [a] -> [(Int, a)]
+enumerate xs = zip [0..] xs
