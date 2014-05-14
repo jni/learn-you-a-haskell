e50ce0a4227271399b589d20c2ca9fcb5154f67b
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-05-14 17:35:23 +1000

    Add zipWith function

--- baby.hs
+++ baby.hs
@@ -29,3 +29,8 @@ mytake n (x:xs)
     | n <= 0    = []
     | otherwise = x : mytake (n-1) xs
 
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
