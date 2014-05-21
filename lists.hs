52ca9223c513e00afd979265a877c8b604e0bb4d
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-05-21 16:27:33 +1000

    Add foldr-based partition function

--- lists.hs
+++ lists.hs
@@ -6,3 +6,11 @@ concatMap' f = L.concat . (map f)
 concatMap'' :: (a -> [b]) -> [a] -> [b]
 concatMap'' _ [] = []
 concatMap'' f (x:xs) = f x ++ concatMap'' f xs
+
+partition' :: (a -> Bool) -> [a] -> ([a], [a])
+partition' p = foldr (place p) ([], [])
+             where
+                place :: (a -> Bool) -> a -> ([a], [a]) -> ([a], [a])
+                place p x (xs, ys)
+                    | p x       = (x:xs, ys)
+                    | otherwise = (xs, x:ys)
